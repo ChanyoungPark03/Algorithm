a8e6106662e28d5bccafc5bf91641d41a835c0b3
ChanyoungPark <channy0210@naver.com>
2025-11-05 15:48:04 +0900

    [Bronze V] Title: ??!, Time: 172 ms, Memory: 17664 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,16 @@
+//10926. ??!
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+
+        //입력
+        Scanner sc = new Scanner(System.in);
+
+        //문자열 입력받기(id)
+        String id = sc.next();
+
+        //문자열 출력 후 ??! 붙이기
+        System.out.println(id + "??!");
+    }
+}
