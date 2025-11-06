d4f5b3f3f517fd6b82f34f337e81ea0b8c5be391
ChanyoungPark <channy0210@naver.com>
2025-11-07 02:07:16 +0900

    [Bronze V] Title: 고양이, Time: 92 ms, Memory: 13876 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,11 @@
+//10171. 고양이
+public class Main {
+    public static void main(String[] args){
+
+        // \\의 활용
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
