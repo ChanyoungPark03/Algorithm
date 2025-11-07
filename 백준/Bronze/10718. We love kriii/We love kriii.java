396c70cd9063688ef9d03349f12e7c01e9c55b8e
ChanyoungPark <channy0210@naver.com>
2025-11-07 15:43:01 +0900

    [Bronze V] Title: We love kriii, Time: 104 ms, Memory: 13904 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,10 @@
+//10718. We love kriii
+
+public class Main {
+    public static void main(String[] args){
+
+        //출력
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+    }
+}
