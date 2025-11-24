3ca8c2f27818701edd5d3720a8024b6eff61a304
ChanyoungPark <channy0210@naver.com>
2025-11-24 16:18:56 +0900

    [Bronze V] Title: 콜센터, Time: 100 ms, Memory: 13856 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,17 @@
+// 5339. 콜센터
+// 이스케이프 시퀀스(\) 활용
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("     /~\\");
+        System.out.println("    ( oo|");
+        System.out.println("    _\\=/_");
+        System.out.println("   /  _  \\");
+        System.out.println("  //|/.\\|\\\\");
+        System.out.println(" ||  \\ /  ||");
+        System.out.println("============");
+        System.out.println("|          |");
+        System.out.println("|          |");
+        System.out.println("|          |");
+    }
+}
