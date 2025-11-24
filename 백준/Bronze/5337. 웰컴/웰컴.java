12db8309d558c7f8dc66f2227113fa46b029bfda
ChanyoungPark <channy0210@naver.com>
2025-11-24 16:06:55 +0900

    [Bronze V] Title: 웰컴, Time: 96 ms, Memory: 13852 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,12 @@
+// 5337. 웰컴
+// 이스케이프 시퀀스(\) 사용
+
+public class Main {
+    public static void main(String[] args) {
+        
+        // 그림 출력
+        System.out.println(".  .   .");
+        System.out.println("|  | _ | _. _ ._ _  _");
+        System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
