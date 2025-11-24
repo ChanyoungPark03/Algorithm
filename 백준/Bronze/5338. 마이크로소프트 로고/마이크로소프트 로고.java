6b05c8d4a1785a4071f8a597886836a3c9097120
ChanyoungPark <channy0210@naver.com>
2025-11-24 16:12:27 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 100 ms, Memory: 14060 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,12 @@
+// 5338. 마이크로소프트 로고
+// 이스케이프 시퀀스(\) 활용
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("       _.-;;-._");
+        System.out.println("\'-..-\'|   ||   |");
+        System.out.println("\'-..-\'|_.-;;-._|");
+        System.out.println("\'-..-\'|   ||   |");
+        System.out.println("\'-..-\'|_.-\'\'-._|");
+    }
+}
